af0c31653810a5ad8208d24214ed4f3d31205fbf
KwonHyeji <kwonhyeji007@gmail.com>
2024-11-11 15:07:12 +0900

    11/11 in class

--- src/main/java/com/green/board/BoardService.java
+++ src/main/java/com/green/board/BoardService.java
@@ -1,4 +1,17 @@
 package com.green.board;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+/*
+    @Service - 빈 등록
+
+    빈 등록: 스프링 컨테이너에게 객체 생성을 대리로 맡기는 것, 기본적으로
+            싱글톤(객체 생성을 단 하나만 되게 하는 것)으로 객체화
+ */
+
+@Service
+@RequiredArgsConstructor
 public class BoardService {
+    private BoardMapper mapper; //DI 받을 수 있게 세팅해 주세요.
 }
+
--- src/main/java/com/green/board/model/BoardInsReq.java
+++ src/main/java/com/green/board/model/BoardInsReq.java
@@ -1,5 +1,12 @@
 package com.green.board.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
 public class BoardInsReq {
     private String title;
     private String contents;
